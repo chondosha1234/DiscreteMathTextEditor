efee63c675920da273162ab3b1831b5c3128c2a9
chondosha1234 <jmiller61193@gmail.com>
2023-09-21 14:17:31 -0400

    got numpad7 to work for conjunction

--- src/DiscreteMathEditor.java
+++ src/DiscreteMathEditor.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import javax.swing.text.DefaultEditorKit;
+//import javax.swing.text.DefaultEditorKit;
 import java.awt.event.*;
 
 
@@ -16,9 +16,9 @@ public class DiscreteMathEditor extends JFrame {
         getContentPane().add(new JScrollPane(textArea));
 
         //KeyStroke customeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK);
-        KeyStroke andKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD7, 0);
+        //KeyStroke andKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD7, 0);
 
-
+        /* 
         //textArea.getInputMap().put(customeKeyStroke, "customAction");
         textArea.getInputMap().put(andKeyStroke, "andAction");
         textArea.getActionMap().put("andAction", new AbstractAction() {
@@ -28,6 +28,35 @@ public class DiscreteMathEditor extends JFrame {
                 currentText += "\u2227";
                 textArea.setText(currentText);
             }
+        });
+        */
+
+        textArea.addKeyListener(new KeyListener(){
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
+                    int caretPosition = textArea.getCaretPosition();
+                    System.out.println(caretPosition);
+                    textArea.insert("\u2227", caretPosition);
+                    int newCaretPosition = textArea.getCaretPosition();
+                    System.out.println(newCaretPosition);
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
+                    String text = textArea.getText();
+                    String newText = text.substring(0, text.length()-1);
+                    textArea.setText(newText);
+                }
+            }
+
         });
     }
 }
