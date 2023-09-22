423865aa8fef8ed4af820476ba979ad26fd5b668
chondosha1234 <jmiller61193@gmail.com>
2023-09-22 19:11:40 -0400

    fixed not sign code and changed font and margins

--- src/DiscreteMathEditor.java
+++ src/DiscreteMathEditor.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
-//import javax.swing.text.DefaultEditorKit;
+
+import java.awt.Font;
+import java.awt.Insets;
 import java.awt.event.*;
 
 
@@ -13,50 +15,94 @@ public class DiscreteMathEditor extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         textArea = new JTextArea();
+        System.out.println(textArea.getFont());
+
+        Font font = new Font("Dialog", Font.PLAIN, 24);
+        textArea.setFont(font);
+
+        Insets margin = new Insets(10,10,10,10);
+        textArea.setMargin(margin);
+
         getContentPane().add(new JScrollPane(textArea));
-
-        //KeyStroke customeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK);
-        //KeyStroke andKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD7, 0);
-
-        /* 
-        //textArea.getInputMap().put(customeKeyStroke, "customAction");
-        textArea.getInputMap().put(andKeyStroke, "andAction");
-        textArea.getActionMap().put("andAction", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String currentText = textArea.getText();
-                currentText += "\u2227";
-                textArea.setText(currentText);
-            }
-        });
-        */
 
         textArea.addKeyListener(new KeyListener(){
 
             @Override
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
-                    int caretPosition = textArea.getCaretPosition();
-                    System.out.println(caretPosition);
+
+                int caretPosition = textArea.getCaretPosition();
+                System.out.println(e.isControlDown());
+
+                if (!e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
+
                     textArea.insert("\u2227", caretPosition);
-                    int newCaretPosition = textArea.getCaretPosition();
-                    System.out.println(newCaretPosition);
+
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
+
+                    textArea.insert("\u2228", caretPosition);
+
+                } else if (!e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD8) {
+
+                    textArea.insert("\u2192", caretPosition);
+
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD8) {
+
+                    textArea.insert("\u2194", caretPosition);
+
+                } else if (!e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD9) {
+
+                    textArea.insert("\u2261", caretPosition);
+
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD9) {
+
+                    textArea.insert("\u00AC", caretPosition);
+                    
+                } else if (!e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD4) {
+
+                    textArea.insert("\u2200", caretPosition);
+
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD4) {
+
+                    textArea.insert("\u2203", caretPosition);
+                    
+                } else if (!e.isControlDown() &&e.getKeyCode() == KeyEvent.VK_NUMPAD5) {
+
+                    textArea.insert("\u2228", caretPosition);
+
+                } else if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_NUMPAD5) {
+
+                    textArea.insert("\u2228", caretPosition);
+                    
                 }
+                
             }
 
             @Override
             public void keyTyped(KeyEvent e) {
+                
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
+
+                if (!e.isControlDown() && (
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD7 || 
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD8 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD9 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD4 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD5 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD6 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD1 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD2 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD3 ||
+                    e.getKeyCode() == KeyEvent.VK_NUMPAD0
+                )) {
+
                     String text = textArea.getText();
                     String newText = text.substring(0, text.length()-1);
                     textArea.setText(newText);
                 }
             }
-
         });
     }
 }
